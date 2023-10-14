66acff287b9bf6d6012c1ccf44d424cd849c066b
Aleksey Khaleev <aleksey52nn@gmail.com>
2023-10-14 22:29:07 +0300

    RobotView

--- game/RobotView.py
+++ game/RobotView.py
@@ -8,11 +8,30 @@ import RobotModel
 
 
 class RobotView(QWidget):
-
     # signals
     key_handled = pyqtSignal(int)
 
     def __init__(self, target_model: RobotModel.Model, parent=None):
         QWidget.__init__(self, parent)
         self.__view_model = target_model
+        self.__white, self.__green, self.__yellow, self.__red = [
+            [QImage(f":/images/VC_{col}_{dest}") for dest in ["lt", "rt", "up", "dn"]] for col in
+            ["wt", "gr", "yw", "rd"]]
+        self.__robot_skin = (self.__white, self.__green, self.__yellow, self.__red)
 
+    def paintEvent(self, event):
+        self.draw_robot()
+
+    def keyPressEvent(self, event):
+        self.key_handled(event.key()).emit()
+
+    def draw_robot(self):
+        qp = QPainter(self)
+        qp.drawImage(QRect(self.__view_model.robotPosition.x() * RobotModel.Model.DOT_SIDE,
+                           self.__view_model.robotPosition.y() * RobotModel.Model.DOT_SIDE,
+                           RobotModel.Model.DOT_SIDE, RobotModel.Model.DOT_SIDE),
+                     self.__robot_skin[self.__view_model.curColor][self.__view_model.robotDestination])
+
+    def update_model(self, model: RobotModel.Model):
+        self.__view_model = model
+        self.repaint()
