4aef8bc6d111f5a0d40e903c75b41811996d968b
Aleksey Khaleev <aleksey52nn@gmail.com>
2023-10-15 19:35:01 +0300

    added: Main_App

--- Main_App.py
+++ Main_App.py
@@ -1,12 +1,14 @@
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import Qt
 
 import sys
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setOverrideCursor(Qt.CursorShape.BlankCursor)
 
-    window = MainWindow()
-    window.show()
+    window = QMainWindow()
+    window.showFullScreen()
 
     app.exec()
